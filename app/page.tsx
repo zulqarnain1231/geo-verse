761950aa6381bba2e4842508588580f707ea71db
zulqarnain1231 <zulqarnainniazi447@gmail.com>
2023-10-09 00:07:05 -0700

    made the hamburger menu for mobile view

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ import Navbar from "@/components/Shared/Navigations/Navbar";
 
 export default function Home() {
   return (
-    <main className="w-full h-full bg-[#010A04]">
+    <main className="w-full h-full bg-[#000700]">
       <section
         id="hero"
         className="w-full lg:h-[100dvh] bg-[url('/Assets/Hero/HeroBg.png')] bg-cover bg-center bg-no-repeat"
--- components/Shared/Navigations/Navbar.tsx
+++ components/Shared/Navigations/Navbar.tsx
@@ -1,10 +1,18 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
 import ComponentWrapper from "../Wrappers/ComponentWrapper";
 import { FiSettings, FiMenu } from "react-icons/fi";
+import { RxCross1 } from "react-icons/rx";
+import Drawer from "react-modern-drawer";
+import "react-modern-drawer/dist/index.css";
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const toggleDrawer = () => {
+    setIsOpen((prevState) => !prevState);
+  };
   const menu = [
     {
       name: "Home",
@@ -24,42 +32,78 @@ const Navbar = () => {
     },
   ];
   return (
-    <ComponentWrapper
-      id="navbar"
-      style="w-full h-[80px] bg-black-main/20 backdrop-blur-sm"
-    >
-      <nav className="w-full h-full flex items-center justify-between">
-        <Link href={"/"} className="w-[140px] h-[54px] relative">
-          <Image
-            className="w-full h-full object-fill"
-            src={"/Assets/Logo.png"}
-            alt=""
-            fill
-          />
-        </Link>
-        <div className="h-full flex items-center justify-center gap-8">
-          {menu.map((item: any, index: number) => (
-            <Link
-              key={index}
-              href={item.route}
-              className="text-white-main text-base font-semibold md:inline-block hidden"
-            >
-              {item.name}
-            </Link>
-          ))}
+    <>
+      <ComponentWrapper
+        id="navbar"
+        style="w-full h-[80px] bg-black-main/20 backdrop-blur-sm"
+      >
+        <nav className="w-full h-full flex items-center justify-between">
+          <Link href={"/"} className="w-[140px] h-[54px] relative">
+            <Image
+              className="w-full h-full object-fill"
+              src={"/Assets/Logo.png"}
+              alt=""
+              fill
+            />
+          </Link>
+          <div className="h-full flex items-center justify-center gap-8">
+            {menu.map((item: any, index: number) => (
+              <Link
+                key={index}
+                href={item.route}
+                className="text-white-main text-base font-semibold md:inline-block hidden"
+              >
+                {item.name}
+              </Link>
+            ))}
 
-          <button className="h-[52px] w-[160px] lg:inline-block hidden bg-brand-main rounded-2xl text-white-main text-lg font-bold ">
-            Connect Wallet
-          </button>
-          <button className="lg:inline-block hidden">
-            <FiSettings className="text-white-main text-3xl" />
-          </button>
-          <button className="md:hidden inline-block">
-            <FiMenu className="text-white-main text-2xl" />
-          </button>
+            <button className="h-[52px] w-[160px] lg:inline-block hidden bg-brand-main rounded-2xl text-white-main text-lg font-bold ">
+              Connect Wallet
+            </button>
+            <button className="lg:inline-block hidden">
+              <FiSettings className="text-white-main text-3xl" />
+            </button>
+            <button onClick={toggleDrawer} className="md:hidden inline-block">
+              <FiMenu className="text-white-main text-2xl" />
+            </button>
+          </div>
+        </nav>
+      </ComponentWrapper>
+      <Drawer
+        open={isOpen}
+        onClose={toggleDrawer}
+        direction="top"
+        style={{ height: "100%" }}
+        className="bla bla bla"
+      >
+        <div className="w-full h-full flex flex-col items-center justify-start gap-8 p-4 bg-black-main">
+          <div className="w-full flex items-center justify-end">
+            <button onClick={toggleDrawer}>
+              <RxCross1 className="text-white-main text-2xl" />
+            </button>
+          </div>
+          <div className="w-full flex flex-col items-center justify-center gap-8">
+            {" "}
+            {menu.map((item: any, index: number) => (
+              <Link
+                key={index}
+                onClick={toggleDrawer}
+                href={item.route}
+                className="text-white-main text-base font-semibold"
+              >
+                {item.name}
+              </Link>
+            ))}
+            <button className="h-[52px] w-[160px] bg-brand-main rounded-2xl text-white-main text-lg font-bold ">
+              Connect Wallet
+            </button>
+            <button className="">
+              <FiSettings className="text-white-main text-3xl" />
+            </button>
+          </div>
         </div>
-      </nav>
-    </ComponentWrapper>
+      </Drawer>
+    </>
   );
 };
 
