b23c96eb42cc51d5b01f9c14722452b18b1dff7e
zulqarnain1231 <zulqarnainniazi447@gmail.com>
2023-10-08 21:11:06 -0700

    finished features card

--- components/Home/Features/Features.tsx
+++ components/Home/Features/Features.tsx
@@ -1,22 +1,40 @@
 import ComponentWrapper from "@/components/Shared/Wrappers/ComponentWrapper";
 import React from "react";
 import * as Icons from "@/constants/Svg/Icons";
+import FeatureCard from "./FeatureCard";
+
 const Features = () => {
+  const features = [
+    {
+      title: "Procedural map generation",
+      icon: <Icons.Map fill="" stroke="" ClassName="h-[64px] w-[64px]" />,
+      detail:
+        "Geoverse revolutionizes exploration through dynamic procedural map generation. Picture an ever-unfolding world, tailored uniquely for each path. Traditional games rely on pre-designed maps, limiting your journey.",
+    },
+    {
+      title: "Multilayered Exploration",
+      icon: <Icons.Stack fill="" stroke="" ClassName="h-[64px] w-[64px]" />,
+      detail:
+        "Geoverse introduces a multi-dimensional adventure by incorporating layers within the game map. Players can venture into intricate cave systems, explore vast landscapes, and navigate within various buildings.",
+    },
+    {
+      title: "Skill Mastery",
+      icon: <Icons.Skill fill="" stroke="" ClassName="h-[64px] w-[64px]" />,
+      detail:
+        "Unleash your potential by mastering skills like woodcutting, mining, and crafting. Each skill not only elevates your gameplay but holds a unique place in our expansive world.  Forge your destiny.",
+    },
+  ];
   return (
     <ComponentWrapper id="features" style="lg:py-16 py-12">
-      <div className="w-full h-full grid grid-cols-3 gap-10">
-        <div className="w-full flex flex-col items-center justify-start gap-4">
-          <Icons.Map fill="" stroke="" ClassName="h-[64px] w-[64px]" />
-          <p className="text-center text-white-main text-2xl font-semibold">
-            Procedural map generation
-          </p>
-          <p className="text-white-off text-center text-base font-medium">
-            Geoverse revolutionizes exploration through dynamic procedural map
-            generation. Picture an ever-unfolding world, tailored uniquely for
-            each path. Traditional games rely on pre-designed maps, limiting
-            your journey.
-          </p>
-        </div>
+      <div className="w-full h-full grid lg:grid-cols-3 gap-10">
+        {features.map((item: any, index: number) => (
+          <FeatureCard
+            key={index}
+            title={item.title}
+            icon={item.icon}
+            detail={item.detail}
+          />
+        ))}
       </div>
     </ComponentWrapper>
   );
